d351f2215a1d096c07b48e220f7948386187ae95
Mikhail Nikonov <mikhailnikonov@Mac.home>
2025-02-20 09:45:38 +0100

    add spouse/domestic partner validation tests

--- ApiTests/UnitTests/Services/PaycheckCalculationServiceTests.cs
+++ ApiTests/UnitTests/Services/PaycheckCalculationServiceTests.cs
@@ -4,6 +4,7 @@ using Api.Services.DeductionRules;
 using Api.Services.Interfaces;
 using Api.DataAccess.Interfaces;
 using Api.Dtos.Paycheck;
+using System;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using Moq;
@@ -14,6 +15,8 @@ namespace Api.Tests.Services;
 public class PaycheckCalculationServiceTests
 {
     private readonly Mock<IDeductionConfigRepository> _configRepositoryMock;
+    private readonly Mock<IEmployeeRepository> _employeeRepositoryMock;
+    private readonly Mock<IEmployeeValidationService> _validationServiceMock;
     private readonly DeductionConfig _config;
     private readonly PaycheckCalculationService _service;
     private readonly List<IDeductionRule> _rules;
@@ -21,9 +24,17 @@ public class PaycheckCalculationServiceTests
     public PaycheckCalculationServiceTests()
     {
         _configRepositoryMock = new Mock<IDeductionConfigRepository>();
+        _employeeRepositoryMock = new Mock<IEmployeeRepository>();
+        _validationServiceMock = new Mock<IEmployeeValidationService>();
         _config = new DeductionConfig { PaychecksPerYear = 24 };
         _rules = new List<IDeductionRule>();
-        _service = new PaycheckCalculationService(_rules, _configRepositoryMock.Object);
+        
+        _service = new PaycheckCalculationService(
+            _rules, 
+            _configRepositoryMock.Object,
+            _employeeRepositoryMock.Object,
+            _validationServiceMock.Object
+        );
         
         _configRepositoryMock.Setup(x => x.GetDeductionConfig())
             .ReturnsAsync(_config);
@@ -35,33 +46,39 @@ public class PaycheckCalculationServiceTests
     public async Task CalculatePaycheck_ShouldCalculateCorrectAmounts(decimal salary, decimal annualDeductions)
     {
         // Arrange
-        var employee = new Employee { Salary = salary };
+        var employee = new Employee { Id = 1, Salary = salary };
+        _employeeRepositoryMock.Setup(x => x.GetEmployeeById(1))
+            .ReturnsAsync(employee);
+            
         var mockRule = new Mock<IDeductionRule>();
         mockRule.Setup(r => r.GetDeduction(employee, _config))
             .Returns(annualDeductions);
         _rules.Add(mockRule.Object);
 
         // Act
-        var result = await _service.CalculatePaycheck(employee);
+        var result = await _service.CalculatePaycheck(1);
 
         // Assert
-        Assert.Equal(salary / 24, result.GrossPay);
-        Assert.Equal(annualDeductions / 24, result.Deductions);
-        Assert.Equal((salary - annualDeductions) / 24, result.NetPay);
+        Assert.Equal(salary / _config.PaychecksPerYear, result.GrossPay);
+        Assert.Equal(annualDeductions / _config.PaychecksPerYear, result.Deductions);
+        Assert.Equal((salary - annualDeductions) / _config.PaychecksPerYear, result.NetPay);
     }
 
     [Fact]
     public async Task CalculatePaycheck_WithPennies_ShouldHandleRoundingCorrectly()
     {
         // Arrange
-        var employee = new Employee { Salary = 100000.99m };
+        var employee = new Employee { Id = 1, Salary = 100000.99m };
+        _employeeRepositoryMock.Setup(x => x.GetEmployeeById(1))
+            .ReturnsAsync(employee);
+            
         var mockRule = new Mock<IDeductionRule>();
         mockRule.Setup(r => r.GetDeduction(employee, _config))
             .Returns(10000.99m);
         _rules.Add(mockRule.Object);
 
         // Act
-        var result = await _service.CalculatePaycheck(employee);
+        var result = await _service.CalculatePaycheck(1);
 
         // Assert
         Assert.Equal(4166.89m, result.GrossPay);
@@ -73,7 +90,10 @@ public class PaycheckCalculationServiceTests
     public async Task CalculatePaycheck_WithMultipleRules_ShouldSumDeductions()
     {
         // Arrange
-        var employee = new Employee { Salary = 120000 };
+        var employee = new Employee { Id = 1, Salary = 120000 };
+        _employeeRepositoryMock.Setup(x => x.GetEmployeeById(1))
+            .ReturnsAsync(employee);
+            
         var mockRule1 = new Mock<IDeductionRule>();
         var mockRule2 = new Mock<IDeductionRule>();
         
@@ -83,11 +103,38 @@ public class PaycheckCalculationServiceTests
         _rules.AddRange(new[] { mockRule1.Object, mockRule2.Object });
 
         // Act
-        var result = await _service.CalculatePaycheck(employee);
+        var result = await _service.CalculatePaycheck(1);
 
         // Assert
         Assert.Equal(5000, result.GrossPay);
         Assert.Equal(416.82m, result.Deductions);
         Assert.Equal(4583.18m, result.NetPay);
+    }
+
+    [Fact]
+    public async Task CalculatePaycheck_WithInvalidDependents_ShouldThrowException()
+    {
+        // Arrange
+        var employee = new Employee 
+        { 
+            Id = 1,
+            Salary = 120000,
+            Dependents = new List<Dependent>
+            {
+                new() { Relationship = Relationship.Spouse },
+                new() { Relationship = Relationship.DomesticPartner }
+            }
+        };
+
+        _employeeRepositoryMock.Setup(x => x.GetEmployeeById(1))
+            .ReturnsAsync(employee);
+
+        _validationServiceMock.Setup(x => x.ValidateEmployee(employee))
+            .Throws(new InvalidOperationException("Cannot have both spouse and domestic partner"));
+
+        // Act & Assert
+        await Assert.ThrowsAsync<InvalidOperationException>(
+            () => _service.CalculatePaycheck(1)
+        );
     }
 }
